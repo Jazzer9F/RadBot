Fix IL calculation
Factor 2 mistake fixed

RadBot.py
@@ -182,11 +182,10 @@ class RadBot:
                 expected_eXRD = initial_eXRD / growth_factor
                 fees_USDC += current_USDC - expected_USDC
                 fees_eXRD += current_eXRD - expected_eXRD
-                value_pooled = 2*initial_USDC
-                total_value_pooled += value_pooled
-                IL -= (2*np.sqrt(price_change_factor)/(1+price_change_factor) - 1)*value_pooled
+                total_value_pooled += 2*initial_USDC
+                IL -= (2*np.sqrt(price_change_factor)/(1+price_change_factor) - 1)*initial_USDC
             msg += f"\nFees earned at UniSwap: {round(fees_USDC, 2)} USDC + {round(fees_eXRD, 2)} eXRD"
-            msg += f"\nImpermanent loss: {round(IL, 2)} USDC ({round(100*IL/total_value_pooled)}%)"
+            msg += f"\nImpermanent loss: {round(IL, 2)} USDC equivalent ({round(100*IL/total_value_pooled)}%)"
 
         if len(self.portfolio.stakes): msg += "\n\nStaking details:"
         for i in range(len(self.portfolio.stakes)):
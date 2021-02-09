642b55ed1f5d08e495e365d13e74c08b310d1fe8
Jazzer9F <Jazzer9F@users.noreply.github.com>
2021-02-09 15:04:43 +0100

    Changed to pinned messages
    Now rotates between the pinned messages every 3h

--- forwardAnnouncement.py
+++ forwardAnnouncement.py
@@ -3,16 +3,16 @@
 Created on Wed 3 Feb 2021
 @author: Jazzer
 
-Will read the last 4 announcements from the announcements channel and post one of them in the main group.
+Will read the pinned messages from the announcements channel and post one of them in the main group.
 """
 import json
 import time
 import asyncio
-from telethon import TelegramClient
-from telethon.tl.functions.messages import GetHistoryRequest
+from telethon import TelegramClient, types
 
 ann_channel = 'RadixAnnouncements'
 main_group = 'radix_dlt'
+period = 3     # Post once every this many hours
 
 def loadCredentials():
     with open('./telegram_API_credentials.json') as f:
@@ -22,15 +22,18 @@ def loadCredentials():
     return (api_id, api_hash)
 
 async def forwardAnnouncement():
-    if time.time()%7200 < 1800: return
+    if time.time()%(period*3600) > 1800: return
     api_id, api_hash = loadCredentials()
-    async with TelegramClient('RadNews', api_id, api_hash) as client:
-        await client.start()
+    async with await TelegramClient('RadNews', api_id, api_hash).start() as client:
         channel = await client.get_entity(ann_channel)
-        posts =  await client.get_messages(channel, 4)
-        group = await client.get_entity(main_group)
-        selected = 3-int(time.time()/7200)%4
-        await client.forward_messages(group, posts[selected])
+        posts = []
+        async for post in client.iter_messages(channel, filter=types.InputMessagesFilterPinned()):
+            posts.append(post)
+        n = len(posts)
+        if n > 0:
+            selected = (n-1)-int(time.time()/3600/period)%n
+            group = await client.get_entity(main_group)
+            await client.forward_messages(group, posts[selected])
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()

5e220f0b9c9dace90cedb8633aa710e9a077c919
Jazzer9F <Jazzer9F@users.noreply.github.com>
2021-02-16 15:01:27 +0100

    simultaneous claims bug fix
    Fixed an issue that occured when multiple unstakes happen in a single block.

--- rewards.py
+++ rewards.py
@@ -41,7 +41,8 @@ class RewardTrender():
             data = [totalStake, totalStakeTime, E, U]
             combined = pd.concat(data, axis=1, keys=[s.name for s in data])
             claimed = unstaked.set_index(pd.to_datetime(unstaked['timestamp']*1e9))['claimed']/1e18
-            combined = combined.join(claimed)
+            claimed =  claimed.groupby('timestamp').sum()
+            combined = combined.join(claimed, how='outer')
             combined.claimed = combined.claimed.fillna(0).cumsum()
             combined['remainingU'] = combined.totalU - combined.claimed
             combined['destroyedST'] = self.calcDestroyedStakeTime(staked, unstaked)
@@ -50,7 +51,7 @@ class RewardTrender():
             combined['donatedU'] = self.calcDonatedRewards(combined)
 
             self.stakeData = combined
-        
+
     def loadDataBase(self):
         if self.store_cache[0] is not None:
             return self.store_cache[0], self.store_cache[1]
@@ -79,8 +80,8 @@ class RewardTrender():
             store['unstakedDF'] = unstakedDF
             store.close()
             self.store_cache = [stakedDF, unstakedDF]
-        
-        
+
+
     def updateEventList(self):
         staked, unstaked = self.loadDataBase()
 
@@ -93,42 +94,42 @@ class RewardTrender():
 
         newStaked = self.getStakedEvents(lastBlock+1)
         newUnstaked = self.getUnstakedEvents(lastBlock+1)
-        
+
         if (len(newStaked)==0) & (len(newUnstaked)==0):
             return
-        
+
         self.setAllTimestamps([newStaked, newUnstaked])
 
         allStaked = pd.concat([staked, newStaked]).reset_index(drop=True)
         allUnstaked = pd.concat([unstaked, newUnstaked]).reset_index(drop=True)
-        
+
         self.writeDataBase(allStaked, allUnstaked)
         RewardTrender.__init__(self)
 
-        
+
     def getStakedEvents(self, fromBlockNr):
         stakedFilter = rewardsContract.events.Staked.createFilter(fromBlock=int(fromBlockNr))
         stakedEventList = stakedFilter.get_all_entries()
-        
+
         stakedList = []
         for s in stakedEventList:
             stakedList.append({'user':s.args.user, 'amount':s.args.amount,
                                'total':s.args.total, 'event':'Staked',
                                'hash':s.transactionHash, 'blockNumber':s.blockNumber
                                })
-    
+
         stakedDF = pd.DataFrame(stakedList)
-    
+
         return stakedDF
-    
-    
+
+
     def getUnstakedEvents(self, fromBlockNr):
         unstakedFilter = rewardsContract.events.Unstaked.createFilter(fromBlock=int(fromBlockNr))
         unstakedEventList = unstakedFilter.get_all_entries()
-    
+
         claimedFilter = rewardsContract.events.TokensClaimed.createFilter(fromBlock=int(fromBlockNr))
         claimedEventList = claimedFilter.get_all_entries()
-        
+
         unstakedList = []
         for u,c in zip(unstakedEventList, claimedEventList):
             unstakedList.append({'user':u.args.user, 'amount':u.args.amount,
@@ -136,27 +137,27 @@ class RewardTrender():
                                'hash':u.transactionHash, 'blockNumber':u.blockNumber,
                                'claimed': c.args.amount
                                })
-    
+
         unstakedDF = pd.DataFrame(unstakedList)
-    
+
         return unstakedDF
-    
-    
+
+
     def setAllTimestamps(self, dataFrames):
         blockNrs = set()
         for df in dataFrames:
             if len(df):
                 blockNrs = blockNrs.union(df.blockNumber)
-        
+
         timestamps = {}
         for nr in blockNrs:
             timestamps[nr] = w3.eth.getBlock(nr).timestamp
-        
+
         for df in dataFrames:
             if len(df):
-                df['timestamp'] = df.blockNumber.map(timestamps)          
-    
-    
+                df['timestamp'] = df.blockNumber.map(timestamps)
+
+
     def calcTotalStake(self, stakedDF, unstakedDF):
         unstakedDF = unstakedDF.copy()
         unstakedDF.amount *= -1
@@ -165,18 +166,18 @@ class RewardTrender():
         combined = combined.sort_values(by='timestamp').set_index('timestamp',drop=True)['amount']
         combined.name = 'totalStake'
         combined /= 1e12
-    
+
         return combined.cumsum()
-    
-    
+
+
     def calcTotalStakeTime(self, totalStake):
         diff = (totalStake.index.to_series().diff()//pd.Timedelta('1s')).fillna(0).astype(int)
         dStakeTime = (totalStake.shift()*diff).fillna(0)
         dStakeTime.name = 'totalStakeTime'
-        
+
         return dStakeTime.cumsum()
 
-    
+
     def calcDestroyedStakeTime(self, staked, unstaked):
         staked_ = staked.copy()
         unstaked_ = unstaked.copy()
@@ -192,12 +193,12 @@ class RewardTrender():
                 stake_to_remove -= removed
                 time = u.timestamp - s.timestamp
                 DST += (removed/1e12)*time
-                
+
                 if stake_to_remove <= 0:
                     continue
-            
+
             unstaked_.loc[ix,'destroyed'] = DST
-        
+
         unstaked_ = unstaked_.groupby('timestamp').sum()
         unstaked_ = unstaked_.set_index(pd.to_datetime(unstaked_.index.values*1e9))
         return unstaked_.destroyed
@@ -210,28 +211,28 @@ class RewardTrender():
         totalST = stakeData.remainingST + destroyed
         fullRewards = destroyed/totalST*availableU
         donatedRewards = (fullRewards-claimed).fillna(0)
-        
+
         return donatedRewards
-    
+
 
     def calcEmission(self, t):
         t0 = 1605629336
         t0_ = t0 + (24*60*60*30)
         t1 = 1608221858
-        t1_ = t1 + (24*60*60*30)        
+        t1_ = t1 + (24*60*60*30)
         t2 = 1610822511
         t2_ = t2 + (24*60*60*60)
-        
+
         t3 = t2_
         t3_ = t3 + (24*60*60*30)
         t4 = t3_
         t4_ = t4 + (24*60*60*30)
-    
+
         if type(t) == pd.core.indexes.datetimes.DatetimeIndex:
             t_ = t.copy().astype(int)/1e9
         else:
             t_ = t
-    
+
         E0 = pd.Series(75e6/(24*60*60*30)*(t_>=t0)*(t_<t0_), index=t)
         E1 = pd.Series(50e6/(24*60*60*30)*(t_>=t1)*(t_<t1_), index=t)
         E2 = pd.Series(50e6/(24*60*60*60)*(t_>=t2)*(t_<t2_), index=t)
@@ -239,20 +240,20 @@ class RewardTrender():
         E4 = pd.Series(10e6/(24*60*60*30)*(t_>=t4)*(t_<t4_), index=t)
         E = E0+E1+E2+E3+E4
         E.name = 'emission'
-    
+
         diff = pd.Series(t_, index=t).diff()
         diff[0] = t_[0]-t0
         U = (diff*E).cumsum()
         U.name = 'totalU'
-        
+
         return (E, U)
-    
-    
+
+
     def calcRewardsOverTime(self, stakesDF):
         def calcBonus(t, t0):
             t0_ = pd.Series(pd.Timestamp(t0*1e9), index=t)
             return 1/6 + 5/6*((((t-t0_)/pd.Timedelta('90d'))**2).clip(0,1))
-            
+
         ix = self.stakeData.index
         trendDF = pd.DataFrame()
         for s in stakesDF.itertuples():
@@ -269,11 +270,11 @@ class RewardTrender():
             trendDF[f'donated {s.Index}'] = d.cumsum()*bonus
 
         return trendDF
-    
-    
+
+
     def plotRewards(self, stakesDF):
         trendDF = self.calcRewardsOverTime(stakesDF)
-        
+
         ix = self.baseIndex
         trendDF = trendDF.loc[self.baseIndex]
         past = (ix <= pd.Timestamp(time.time()*1e9))
@@ -296,7 +297,7 @@ class RewardTrender():
         plt.tight_layout()
         plt.savefig(buffer, format='png', bbox='tight')
         plt.close()
-        
+
         buffer.seek(0)
         return buffer
 
@@ -310,4 +311,3 @@ if __name__ == "__main__":
     with rT.plotRewards(stakes) as buffer:
         im = Image.open(buffer)
         im.show()
-
